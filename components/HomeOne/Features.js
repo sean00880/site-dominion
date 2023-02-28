f2558d3d22d280ccc75b09b80c066776f4adc116
sean00880 <theelitereapersanctum@gmail.com>
2023-02-28 10:10:12 -0500

    commit66

--- components/HomeOne/Features.js
+++ components/HomeOne/Features.js
@@ -22,7 +22,7 @@ const Features = () => {
                       
                        
                        <div className="col-lg-4 p-0">
-                            <div className="single-features" data-aos="flip-left"  data-aos-duration="1200" >
+                            <div className="single-features" data-aos="flip-left">
                                 <Image src="/images/design.png" alt="Website Design and development icon." width={100} height={100}/>
                                 <h3>1) Design + Development</h3>
                                 <p> Site Dominion ensures that that your website loads quickly, is safe, functions flawlessly across all browsers and screen sizes, and is set up for the long term to continuously reach your business goals.</p>
@@ -36,7 +36,7 @@ const Features = () => {
                         </div>
 
                         <div className="col-lg-4 offset-lg-0 p-0">
-                            <div className="single-features" data-aos="flip-right"  data-aos-duration="1200" >
+                            <div className="single-features" data-aos="flip-right">
                             <Image src="/images/seo.png" alt="SEO setup icon." width={100} height={100}/>
                                 <h3>2) Project Optimization</h3>
                                 <p>We use SEO and tools, cutting-edge techniques, and experiments to enhance the functionality of your website, further attract more visitors, boost conversions, and increase revenue.</p>
@@ -50,7 +50,7 @@ const Features = () => {
                         </div>
 
                         <div className="col-lg-4 p-0">
-                            <div className="single-features" data-aos="flip-up"  data-aos-duration="1200" data-aos-delay="100">
+                            <div className="single-features" data-aos="flip-up" >
                             <Image src="/images/deployment.png" alt="Website deployment icon." width={100} height={100}/>
                                 <h3>3) Full Deployment [Hosting & Domain]</h3>
                                 <p>After the completion of the developmental phase, the website is pushed into deployment. Site Dominion's premium website package covers all domain and hosting expenses, so you don't have to break a sweat.</p>
@@ -66,7 +66,7 @@ const Features = () => {
                      
                         </div>
                        <hr />
-                       <div className='wrapper2'  data-aos="fade-down">
+                       <div className='wrapper2'>
                             <div>
                                 Browse through our existing projects:
                                 <br />
@@ -92,7 +92,7 @@ const Features = () => {
                 <div className="wrapper">
                     
                         <div className="col-lg-4 p-0">
-                            <div className="single-features" data-aos="flip-left"  data-aos-duration="1200" >
+                            <div className="single-features" data-aos="flip-left" >
                             <Image src="/images/logo-design.png" alt="Logo design icon." width={100} height={100}/>
                                 <h3>Logo Design</h3>
                                 <p>Our goal at Site Dominion is to make the best first impression possible for your brand. One of our design experts will assist you in bringing your brand to life once you reach out to us.</p>
@@ -106,7 +106,7 @@ const Features = () => {
                         </div>
 
                         <div className="col-lg-4 p-0">
-                            <div className="single-features" data-aos="flip-down"  data-aos-duration="1200"  data-aos-delay="100" >
+                            <div className="single-features" data-aos="flip-down" >
                             <Image src="/images/blog-setup.svg" alt="Blog-setup icon." width={100} height={100}/>
                                 <h3>Blog Creation + Setup</h3>
                                 <p>Reach clients and search engines with skilled professionals' high-quality, budget friendly blog and website content. A straightforward solution to cover your blogging needs.</p>
