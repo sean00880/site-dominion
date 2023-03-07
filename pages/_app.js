842bd53efa649642e6603f9de894a4fe022a8486
sean00880 <theelitereapersanctum@gmail.com>
2023-03-07 10:31:59 -0500

    103

--- pages/_app.js
+++ pages/_app.js
@@ -41,9 +41,10 @@ export default class MyApp extends App {
     render () {
         
         
-
+        
         const { Component, pageProps } = this.props
-
+        
+        
         return (
             <>
                 <Head>
@@ -64,8 +65,18 @@ export default class MyApp extends App {
                         name="viewport" 
                         content="width=device-width, initial-scale=1" 
                     />
-                    
+                    <link rel="canonical" href="https://www.sitedominion.com" />
                     <title>Lead Generating Websites for Businesses - https://www.sitedominion.com/</title>
+                    <meta
+          name="description"
+          content="At Site Dominion, we believe that a website is more than just an online presence as it's an extension of your brand and a tool to drive growth and revenue. That's why we offer comprehensive web development services to help businesses of all sizes achieve their online goals."
+          key="desc"
+        />
+        <meta
+          property="og:description"
+          content="Looking for a professional web development agency to take your online presence to the next level? Look no further! Our team of experienced developers can help you create a stunning website that not only looks great but also delivers results."
+        />
+        <meta property="og:image" content="/images/mockup.png" />
                 </Head>
 
                 <Component {...pageProps} />
