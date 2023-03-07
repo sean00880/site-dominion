eea77b5bd92989ec474c1a18059920d6052f986e
sean00880 <theelitereapersanctum@gmail.com>
2023-03-06 19:47:55 -0500

    commit105

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,12 @@ import Document, {
   } from "next/document";
   
   class MyDocument extends Document {
+    
     render() {
+      const siteUrl = 'https://www.sitedominion.com';
+  const { asPath } = useRouter();
+  const cleanPath = asPath.split('#')[0].split('?')[0];
+  const canonicalUrl = `${siteUrl}` + (router.asPath === '/' ? '' : cleanPath);
       return (
         <Html lang="zxx">
           <Head>
@@ -17,6 +22,7 @@ import Document, {
             />
             <link href="https://fonts.googleapis.com/css2?family=montserrat&family=Smooch+Sans&display=swap" rel="stylesheet" />
             <link rel="icon" type="image/png" href="/images/favicon.png"></link>
+            <link rel="canonical" href={canonicalUrl} />
           </Head>
           
           <body>
