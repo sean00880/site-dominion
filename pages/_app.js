41151d4c3412d5ca7f1a109db0c61c8471d795a6
sean00880 <theelitereapersanctum@gmail.com>
2023-03-07 20:35:32 -0500

    106

--- pages/_app.js
+++ pages/_app.js
@@ -16,7 +16,7 @@ import App from 'next/app';
 import Head from 'next/head';
 import Loader from '../components/Shared/Loader';
 import GoTop from '../components/Shared/GoTop';
-
+import { NextSeo } from "next-seo";
 import Script from 'next/script';
 
 
@@ -47,12 +47,34 @@ export default class MyApp extends App {
         
         return (
             <>
-                <Head>
-                <Script
-        src="https://www.googletagmanager.com/gtag/js?id=NEXT_PUBLIC_GOOGLE_ANALYTICS"
-        strategy="afterInteractive"
+                <NextSeo
+        title="Site Domininon - Web Development Services"
+        titleTemplate="Site Domininon - Web Development Services"
+        defaultTitle="Site Domininon - Web Development Services"
+        description="Looking for a lead generating website agency that can help you increase your online presence and drive more traffic to your business? Look no further than Site Dominion's expert team of website developers and digital marketing specialists."
+        canonical="https://www.sitedominion.com/"
+        openGraph={{
+          url: "https://www.sitedominion.com/",
+          title: "Site Dominion - Web Development Services",
+          description: "Looking for a lead generating website agency that can help you increase your online presence and drive more traffic to your business? Look no further than Site Dominion's expert team of website developers and digital marketing specialists.",
+          images: [
+            {
+              url: "/images/lion4.png",
+              width: 270,
+              height: 280,
+              alt: "Site Dominion - Lion Logo",
+            },
+          ],
+        }}
+        twitter={{
+          handle: "@sitedominion",
+          site: "@sitedominion",
+          cardType: "summary_large_image",
+        }}
+       
       />
-      <Script id="google-analytics" strategy="afterInteractive">
+            <Head>
+            <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
@@ -61,24 +83,7 @@ export default class MyApp extends App {
           gtag('config', 'NEXT_PUBLIC_GOOGLE_ANALYTICS');
         `}
       </Script>
-                    <meta 
-                        name="viewport" 
-                        content="width=device-width, initial-scale=1" 
-                    />
-                    <link rel="canonical" href="https://www.sitedominion.com" />
-                    <title>Lead Generating Websites for Businesses - https://www.sitedominion.com/</title>
-                    <meta
-          name="description"
-          content="At Site Dominion, we believe that a website is more than just an online presence as it's an extension of your brand and a tool to drive growth and revenue. That's why we offer comprehensive web development services to help businesses of all sizes achieve their online goals."
-          key="desc"
-        />
-        <meta
-          property="og:description"
-          content="Looking for a professional web development agency to take your online presence to the next level? Look no further! Our team of experienced developers can help you create a stunning website that not only looks great but also delivers results."
-        />
-        <meta property="og:image" content="/images/mockup.png" />
-                </Head>
-
+            </Head>
                 <Component {...pageProps} />
                 
                 {/* Preloader */}
@@ -90,3 +95,5 @@ export default class MyApp extends App {
         );
     }
 }
+
+
